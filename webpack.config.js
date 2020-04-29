903167e823aa4b46d84e257774db3ced373b0b8e
wuxingheng <1228678518@qq.com>
2020-04-29 09:01:42 +0800

    :pushpin: 初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'lib.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
