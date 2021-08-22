54b8658bc908cd9f355c914c19ba53f5d8e51920
wxingheng <wxingheng@outlook.com>
2021-08-22 13:42:09 +0800

    feat: webpack 测试使用

--- example/base/src/index.js
+++ example/base/src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: wuxh
  * @Date: 2020-04-29 10:01:50
- * @LastEditTime: 2020-05-14 14:18:47
+ * @LastEditTime: 2021-08-22 13:31:03
  * @LastEditors: wuxh
  * @Description:
  * @FilePath: /jcommon/example/base/src/index.js
@@ -10,10 +10,13 @@
 // import { cArrDoubleRanking } from 'jcommon/array';
 
 // import { isObject } from 'jcommon'
-import {  } from '../../../dist/index.min'
+// import {  } from '../../../dist/index.min'
 
-console.log(isObject)
+// console.log(isObject)
 
-console.log('isObject', isObject({}))
-console.log('isObject', isObject([]))
+// console.log('isObject', isObject({}))
+// console.log('isObject', isObject([]))
 
+import { isArray } from 'jcommon'
+
+console.log('isArray==>', isArray([]))
--- example/base/webpack.config.js
+++ example/base/webpack.config.js
@@ -1,4 +1,14 @@
+/*
+ * @Author: wuxh
+ * @Date: 2021-08-22 12:46:08
+ * @LastEditTime: 2021-08-22 13:40:42
+ * @LastEditors: wuxh
+ * @Description: 
+ * @FilePath: /jcommon/example/base/webpack.config.js
+ */
 module.exports = {
+  mode: 'production',
+  // mode: 'development',
   entry: __dirname + '/src/index.js', //入口文件
   output: {
     path: __dirname + '/public', //打包后的文件存放目录
